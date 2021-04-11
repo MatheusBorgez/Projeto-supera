a165fa142026290e3cd00cdf07d32392dc717c3b
MatheusBorgez <matheusab594@gmail.com>
2021-04-11 06:19:17 -0300

    Adiciona controllers

---
+++ WebAPI/WebAPI/Controllers/MenuController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebAPI.Models;
+using WebAPI.Processos;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MenuController : ControllerBase
+    {
+        [HttpPost]
+        public void ImportArquivo(string caminhoArquivo)
+        {
+            var produtos = new ProcessoArquivos().ImporteProdutos(caminhoArquivo);
+        }
+    }
+}
