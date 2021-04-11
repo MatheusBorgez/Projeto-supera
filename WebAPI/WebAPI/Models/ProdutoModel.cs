1298a63f0acba0d73183c547e1ea447c9313a125
MatheusBorgez <matheusab594@gmail.com>
2021-04-11 06:23:37 -0300

    Cria models

---
+++ WebAPI/WebAPI/Models/ProdutoModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class ProdutoModel
+    {
+
+        public int IdProduto { get; set; }
+
+        public int IdUsuario { get; set; }
+
+        public string Descricao { get; set; }
+
+        public decimal PrecoCusto { get; set; }
+
+        public decimal PrecoVenda { get; set; }
+
+        public string Ncm { get; set; }
+
+        public string Referencia { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+    }
+}
